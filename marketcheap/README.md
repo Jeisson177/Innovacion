853198d915abfb0ff4de7b3803e00794f9728dce
Jeisson177 <79467624+Jeisson177@users.noreply.github.com>
2025-03-02 18:09:26 -0500

    flutter

---
+++ marketcheap/README.md
@@ -0,0 +1,3 @@
+# marketcheap
+
+A new Flutter project.
---
+++ marketcheap/android/app/src/main/kotlin/com/example/marketcheap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.marketcheap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
